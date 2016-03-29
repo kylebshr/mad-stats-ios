921aa2032ad5985274d328b30363c48ef948d626
Kyle Bashour <kylebshr@me.com>
2016-03-28 22:25:39 -0400

    Add basic UI tests

--- Mad StatsUITests/Mad_StatsUITests.swift
+++ Mad StatsUITests/Mad_StatsUITests.swift
@@ -8,29 +8,40 @@
 
 import XCTest
 
+// I'm new to UI tests — these pass on iPhone, but not iPad :(
 class Mad_StatsUITests: XCTestCase {
-        
+
+    let app = XCUIApplication()
+
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        continueAfterFailure = false
+
+        app.launch()
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testUI() {
+
+        // Make sure we have the table and the right number of cells
+        XCTAssertEqual(app.tables.count, 1)
+        XCTAssertEqual(app.tables.cells.count, UInt(DataGenerator.games.count))
+
+        // Tap the first cell
+        app.tables.elementBoundByIndex(0).cells.elementBoundByIndex(0).tap()
+
+        // Make sure we have another table and the right number of cells
+        XCTAssertEqual(app.tables.count, 2)
+        XCTAssertEqual(app.tables.elementBoundByIndex(0).cells.count, UInt(DataGenerator.statCount))
+
+        // Tap the dimsiss button
+        app.navigationBars["Season Statistics"].buttons["Stop"].tap()
+
+        // Make sure we're back to one table
+        XCTAssertEqual(app.tables.count, 1)
     }
-    
 }
